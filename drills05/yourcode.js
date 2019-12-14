ea4c2129483bc280fbeef7d980d6b2dd3b695d13
jinho6225 <jinho6225@hotmail.com>
2019-12-14 01:22:17 -0800

    progress..

--- drills05/yourcode.js
+++ drills05/yourcode.js
@@ -1,7 +1,25 @@
 
 
-function populateRecords(){
+function populateRecords(obj){
+  tableA(obj);
+  let newObj = {};
+  var totalCharge = 0;
+  var totalCash = 0;
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].type === 'charge') {
+      totalCharge = totalCharge + Number(obj[i].amount);
+    }
+    if (obj[i].type === 'cash advance') {
+      totalCash = totalCash + Number(obj[i].amount);
+    }
 
+    newObj['charge'] = totalCharge;
+    newObj['cash advance'] = totalCash;
+  }
+  return newObj
 }
 
 
+function tableA(objList) {
+
+}
