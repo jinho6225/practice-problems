65fa549cad9736e35fc1358e460a4c17c3176e26
jinho6225 <jinho6225@hotmail.com>
2019-12-17 01:13:46 -0800

    progress

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,23 @@
-
-
-function removeClassFromElement(){
-	
+function removeClassFromElement(ele, cls){
+  var a = $(ele)
+  console.log(a, cls)
+  a.removeClass(cls)
 }
 
-function toggleClassOnElement(){
-	
+function toggleClassOnElement(ele, cls){
+  var b = $(ele)
+  console.log(b, cls)
+  b.toggleClass(cls)
 }
 
 function hideElements(  ){
-	
+
 }
 
 function addAttributeToElement( ){
-	
+
 }
 
 function putPosInElement(  ){
-	
+
 }
-
-
